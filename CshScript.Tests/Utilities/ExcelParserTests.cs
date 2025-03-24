8d159ea07f1aad14193ebcf6045f581784df6d63
David Ajken <david.ajken@hotmail.com>
2025-03-24 13:24:28 +0100

    Update ExcelParserTests.cs

--- CshScript.Tests/Utilities/ExcelParserTests.cs
+++ CshScript.Tests/Utilities/ExcelParserTests.cs
@@ -4,15 +4,16 @@ using CshScript.Utilities;
 using CshScript.Tests.TestUtils;
 using CshScript.Tests.TestUtilitys;
 
-public class ExcelParserTests {
+public class ExcelParserTests
+{
 
     [Fact]
     public void ParseExcel()
     {
         // Given
-        var path = Path.Combine(SlnPath.TryGetSolutionDirectoryInfo().FullName, "CshScript.Tests\\Resourses\\pdf.xlsx")  ;
+        var path = Path.Combine(SlnPath.TryGetSolutionDirectoryInfo().FullName, "CshScript.Tests\\Resourses\\pdf.xlsx");
         var numberOfRows = 2;
-            
+
         // When
         var actual = ExcelParser.ParseExcel(path, numberOfRows);
         Thread.Sleep(10);
@@ -26,9 +27,14 @@ public class ExcelParserTests {
     public void ExcelNotFound()
     {
         // Given
-    
+        string path = "notfound.xlsx";
+        var numberOfRows = 2;
         // When
-    
+
         // Then
+        Assert.Throws<FileNotFoundException>(() =>
+        {
+            var actual = ExcelParser.ParseExcel(path, numberOfRows);
+        });
     }
 }
